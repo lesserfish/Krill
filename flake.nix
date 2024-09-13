54ffae5edda04c301ac8f0f38efdf4dd1bb24469
lesserfish <lesserfish@pm.me>
2024-09-12 23:35:08 -0300

    Updated flake

--- flake.nix
+++ flake.nix
@@ -3,27 +3,46 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-  }; 
-  outputs = { self, nixpkgs, ... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in pkgs.mkShell {
-      packages = with pkgs; [
-        bashInteractive 
-        ghc
-        cabal-install
-        haskell-language-server
-        SDL2
-      ];
-      nativeBuildInputs = with pkgs; [
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    devShells = {
+      x86_64-linux.default = let
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          ghc
+          cabal-install
+          haskell-language-server
+          SDL2
+        ];
+        nativeBuildInputs = with pkgs; [
           pkg-config
-      ];
-     };
+        ];
+      };
+
+      aarch64-darwin.default = let
+        pkgs = import nixpkgs {
+          system = "aarch64-darwin";
+          config.allowUnfree = true;
+        };
+      in pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          ghc
+          cabal-install
+          haskell-language-server
+          SDL2
+        ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
+      };
+    };
   };
 }
+
